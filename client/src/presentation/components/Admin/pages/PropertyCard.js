4f3f88f015a2211288f751299c0da9349d46cb0d
idamanukyan <ida_manukyan@edu.aua.am>
2024-10-11 17:40:05 +0400

    cleaned main page code and logs page

--- client/src/presentation/components/Admin/pages/PropertyCard.js
+++ client/src/presentation/components/Admin/pages/PropertyCard.js
@@ -1,25 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './style/PropertyCard.css';
 import mockPhoto from "../pages/style/public/property-mock-photo.jpeg";
 
 const PropertyCard = ({ property }) => {
     const navigate = useNavigate();
-    const [primaryPhotoUrl, setPrimaryPhotoUrl] = useState('');
+        const [primaryPhotoUrl, setPrimaryPhotoUrl] = useState('');
 
-/*    useEffect(() => {
-        const fetchPrimaryPhoto = async () => {
-            try {
-                const response = await axios.get(`http://localhost:8080/api/properties/photos/primary/${property.propertyId}`);
-                setPrimaryPhotoUrl(response.data.photoUrl); // Adjust this based on your actual API response
-            } catch (error) {
-                console.error('Error fetching primary photo:', error);
-            }
-        };
-
-        fetchPrimaryPhoto();
-    }, [property]);*/
 
     const handleSeeMoreClick = () => {
         if (property && property.id) { // Ensure property exists before accessing its ID
@@ -32,15 +20,15 @@ const PropertyCard = ({ property }) => {
     }
 
     return (
-        <div className="property-card"
-             style={{backgroundImage: `url(${primaryPhotoUrl || mockPhoto})`}}
+        <div
+            className="property-card"
+            style={{ backgroundImage: `url(${primaryPhotoUrl || mockPhoto})` }}
         >
             <div className="property-card-content">
-
                 <h3>{property.propertyName || 'Unnamed Property'}</h3>
                 <p><strong>Typen:</strong> {property.propertyType || 'N/A'}</p>
                 <p><strong>Adresse:</strong> {property.address || 'N/A'}</p>
-                <p><strong>Standort:</strong> {(property.city && property.city.name) ? property.city.name : 'N/A'}, {property.country || 'N/A'}</p>
+                <p><strong>Standort:</strong> {property.city?.name || 'N/A'}, {property.country || 'N/A'}</p>
                 <p><strong>Preis:</strong> €{property.price || 'N/A'}</p>
                 <p><strong>Bewertungen:</strong> {property.rating || 'No ratings'}</p>
                 <p><strong>Maximale Gästezahl:</strong> {property.numberOfGuests || 'N/A'}</p>
