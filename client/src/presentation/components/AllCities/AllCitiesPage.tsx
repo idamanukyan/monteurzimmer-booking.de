0a7f89c3cce4d0fb2549d766dd33e15749cee702
idamanukyan <ida_manukyan@edu.aua.am>
2024-10-23 17:02:44 +0400

    search in all cities

--- client/src/presentation/components/AllCities/AllCitiesPage.tsx
+++ client/src/presentation/components/AllCities/AllCitiesPage.tsx
@@ -4,9 +4,11 @@ import { FavCitiesModel } from "../../../data/model/FavCitiesModel";
 import { useNavigate } from "react-router-dom";
 import { FavCitiesService } from '../../../data/service/FavCitiesService';
 import Header from "../Header";
+import Footer from "../Footer";
 
 const AllCities: React.FC = () => {
     const [favCities, setFavCities] = useState<FavCitiesModel | null>(null);
+    const [searchInput, setSearchInput] = useState<string>(''); // State for search input
     const navigate = useNavigate();
     const favCitiesService = new FavCitiesService();
 
@@ -23,12 +25,29 @@ const AllCities: React.FC = () => {
         return <div>Loading...</div>; // You can customize the loading state
     }
 
+    // Function to handle the search input change
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchInput(event.target.value);
+    };
+
+    // Filter cities based on the search input
+    const filteredCities = favCities.filter(city =>
+        city.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+
     return (
         <div className="container">
             <Header />
             <h2 className="title">Alle Städte</h2> {/* Title for the cities section */}
+            <input
+                type="text"
+                placeholder="Städte suchen..."
+                value={searchInput}
+                onChange={handleSearchChange}
+                className="searchInput" // Add a class for styling
+            />
             <div className="citiesContainer">
-                {favCities.map((city) => (
+                {filteredCities.map((city) => (
                     <div
                         key={city.id}
                         className="cityBox" // Use plain class names
@@ -40,6 +59,7 @@ const AllCities: React.FC = () => {
                     </div>
                 ))}
             </div>
+            <Footer/>
         </div>
     );
 };
