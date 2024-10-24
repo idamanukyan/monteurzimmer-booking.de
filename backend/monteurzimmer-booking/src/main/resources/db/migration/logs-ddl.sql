216ff116ed5e8fbbcdb0efa723b1686553ebc974
idamanukyan <ida_manukyan@edu.aua.am>
2024-10-24 12:59:07 +0400

    database updated

--- backend/monteurzimmer-booking/src/main/resources/db/migration/logs-ddl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE logs
-(
-    id        BIGINT AUTO_INCREMENT PRIMARY KEY,
-    timestamp DATETIME     NOT NULL,
-    level     VARCHAR(10)  NOT NULL,
-    message   VARCHAR(255) NOT NULL
-);
