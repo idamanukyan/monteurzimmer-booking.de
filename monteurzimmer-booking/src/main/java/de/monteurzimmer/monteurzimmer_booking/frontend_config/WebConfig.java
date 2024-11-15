d6e73e565ef5e6c4feed9ea1c2b5064d35fde84a
idamanukyan <ida_manukyan@edu.aua.am>
2024-11-15 13:29:07 +0400

    cross

--- monteurzimmer-booking/src/main/java/de/monteurzimmer/monteurzimmer_booking/frontend_config/WebConfig.java
+++ monteurzimmer-booking/src/main/java/de/monteurzimmer/monteurzimmer_booking/frontend_config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://check-monteruzimmer.de")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
