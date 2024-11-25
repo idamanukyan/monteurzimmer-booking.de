413557276d8977fc54f6aeb192957282c558fd0c
idamanukyan <ida_manukyan@edu.aua.am>
2024-11-26 02:47:51 +0400

    Fix CORS issue

---
+++ monteurzimmer-booking/src/main/java/de/monteurzimmer/monteurzimmer_booking/frontend_config/CorsConfig.java
@@ -0,0 +1,32 @@
+package de.monteurzimmer.monteurzimmer_booking.frontend_config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "http://check-monteruzimmer.de",
+                "http://checkmonteurzimmer.de",
+                "https://check-monteurzimmer-frontend-6f3b50cb8e29.herokuapp.com"
+        ));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+    }
+}
+
