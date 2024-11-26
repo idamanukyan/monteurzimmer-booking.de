5f543e848acd151e5e61e565d0e6e40ec88e27d5
idamanukyan <ida_manukyan@edu.aua.am>
2024-11-26 14:52:10 +0400

    Temporary: Allow all CORS origins for debugging

--- src/main/java/de/monteurzimmer/monteurzimmer_booking/frontend_config/WebConfig.java
+++ src/main/java/de/monteurzimmer/monteurzimmer_booking/frontend_config/WebConfig.java
@@ -9,8 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+        //registry.addMapping("/api/**")
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000",
+                        "behavioural-journey-ezkggsrfj4miasdnnfqnxc1x.herokudns.com",
+                        "https://check-monteurzimmer-frontend-6f3b50cb8e29.herokuapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
