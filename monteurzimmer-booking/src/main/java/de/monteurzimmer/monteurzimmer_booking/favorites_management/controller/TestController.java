d2226f520ad8caac53e90814fb860da5e74e666b
idamanukyan <ida_manukyan@edu.aua.am>
2025-01-16 11:53:00 +0100

    test

---
+++ monteurzimmer-booking/src/main/java/de/monteurzimmer/monteurzimmer_booking/favorites_management/controller/TestController.java
@@ -0,0 +1,14 @@
+package de.monteurzimmer.monteurzimmer_booking.favorites_management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Backend is working!";
+    }
+}
